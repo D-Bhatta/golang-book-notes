fe02e8d859a18752e1e90806d6fa8f563c720b9a
D-Bhatta <dbhatta1232@gmail.com>
2021-06-03 07:50:20 -0400

    ref(gbpro): Add program for types

---
+++ books/gobook.pdf/programs/apr_28/types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Types
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("1 + 1 = ", 1.0+1.0)
+	fmt.Println("32132 x 42452 = ", 32132*42452)
+	fmt.Println("Length of 'Hello World' = ", len("Hello World"))
+	fmt.Println("Hello World"[2])
+	fmt.Println("Hello" + "World")
+	fmt.Println(true && true, true && false, true || true, true || false, !true)
+
+}
