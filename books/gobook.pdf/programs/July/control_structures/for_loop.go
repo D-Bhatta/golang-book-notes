f37d71bd77bc665b2be8dbbba36181f398accd2e
D-Bhatta <dbhatta1232@gmail.com>
2021-07-14 12:35:55 -0400

    feat(gbpro): Create program for for loop

---
+++ books/gobook.pdf/programs/July/control_structures/for_loop.go
@@ -0,0 +1,15 @@
+//  for_loop.go
+package main
+
+import "fmt"
+
+func main(){
+    // i := 1
+    // for i <= 10 {
+    //     fmt.Println(i)
+    //     i = i + 1
+    // }
+    for j := -5; j <= 0; j++ {
+        fmt.Println(j)
+    }
+}
